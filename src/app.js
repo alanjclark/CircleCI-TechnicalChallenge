17cdd73c96cdfaebc6569da1ea8bde51e1b022d0
Alan Clark <al@Als-iMac.localdomain>
2020-09-24 22:36:34 +0100

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+var http = require('http');
+http.createServer(onRequest).listen(8001);
+
+function onRequest(request, response){
+    response.writeHead(200, {'Content-Type': 'text/html'});   // Sets header info
+    response.write('<div style="text-align: center;">');
+    response.write('<h2>CircleCI Technical Challenge</h2>');
+    response.write('<button type="button" onClick="getElementById(\'click\').innerHTML = \'Clickity click!\'">CLICK THIS!</button>');
+    response.write('<p id="click"></p>');
+    response.end();
+}
