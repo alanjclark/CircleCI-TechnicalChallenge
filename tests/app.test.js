7e0dcb6cc679740e02d6d0a0fe0fcf4e9a33a017
Alan Clark <al@Als-iMac.localdomain>
2020-09-25 11:35:47 +0100

    Corrects minor errors in test

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,12 +1,13 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
+var assert = require('assert');
 
 (async function buttonClickTest() {
     let driver = await new Builder().forBrowser('chrome').build();
     try {
         await driver.get('http://localhost:8001');
-        await driver.findElement(By.id('button')).click;
+        await driver.findElement(By.id('button')).click();
         var message = await driver.findElement(By.id('click')).getText();
-        assert.equal(message, "Clickity click!")
+        assert.strictEqual(message, "Clickity click!")
     } finally {
         await driver.quit();
     }
