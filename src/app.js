b8196df4feb85e94dcb7a4628b3370064176b6f5
Alan Clark <al@Als-iMac.localdomain>
2020-09-25 13:45:09 +0100

    Corrects functions in app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-http.createServer(function (req, res) {
+http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/html'});   // Sets header info
     response.write('<div style="text-align: center;">');
     response.write('<h2>CircleCI Technical Challenge</h2>');
