41a9b5ca9cf47e6acc2920a6215819dc63775933
Alan Clark <al@Als-iMac.localdomain>
2020-09-25 11:30:55 +0100

    Implements test

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ function onRequest(request, response){
     response.writeHead(200, {'Content-Type': 'text/html'});   // Sets header info
     response.write('<div style="text-align: center;">');
     response.write('<h2>CircleCI Technical Challenge</h2>');
-    response.write('<button type="button" onClick="getElementById(\'click\').innerHTML = \'Clickity click!\'">CLICK THIS!</button>');
+    response.write('<button id="button" type="button" onClick="getElementById(\'click\').innerHTML = \'Clickity click!\'">CLICK THIS!</button>');
     response.write('<p id="click"></p>');
     response.end();
 }
