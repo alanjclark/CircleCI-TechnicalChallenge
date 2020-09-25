327b28c421f40f3bcff6d830d51a02021f1615c9
Alan Clark <al@Als-iMac.localdomain>
2020-09-25 13:36:54 +0100

    Changes NPM run method

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 var http = require('http');
-http.createServer(onRequest).listen(8001);
 
-function onRequest(request, response){
+http.createServer(function (req, res) {
     response.writeHead(200, {'Content-Type': 'text/html'});   // Sets header info
     response.write('<div style="text-align: center;">');
     response.write('<h2>CircleCI Technical Challenge</h2>');
     response.write('<button id="button" type="button" onClick="getElementById(\'click\').innerHTML = \'Clickity click!\'">CLICK THIS!</button>');
     response.write('<p id="click"></p>');
     response.end();
-}
+}).listen(8001);
