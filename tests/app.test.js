344e6e62249360333af180cbe15c62f4155caeeb
Alan Clark <al@Als-iMac.localdomain>
2020-09-25 13:21:31 +0100

    Changes Chrome to Firefox in test

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,7 +2,7 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
 var assert = require('assert');
 
 (async function buttonClickTest() {
-    let driver = await new Builder().forBrowser('chrome').build();
+    let driver = await new Builder().forBrowser('firefox').build();
     try {
         await driver.get('http://localhost:8001');
         await driver.findElement(By.id('button')).click();
