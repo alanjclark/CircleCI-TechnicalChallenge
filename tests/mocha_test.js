fc04b28ddba45dcf83270ca92355a3000268de97
Alan Clark <al@Als-iMac.localdomain>
2020-09-27 13:34:45 +0100

    Changes test framework to Mocha.

---
+++ tests/mocha_test.js
@@ -0,0 +1,21 @@
+var assert = require('assert');
+const {Builder, By, Key, until} = require('selenium-webdriver');
+
+describe('Web page', function(){
+    it('should display text on button click', function(){
+        (async function buttonClickTest() {
+            let driver = await new Builder().forBrowser('firefox').build();
+            try {
+                await driver.get('http://localhost:8001');
+                await driver.findElement(By.id('button')).click();
+                var message = await driver.findElement(By.id('click')).getText();
+                assert.strictEqual(message, "Clickity click!");
+            } catch {
+                assert("Button click failed");
+            } finally {
+                await driver.quit();
+            }
+        })();
+    })
+})
+
