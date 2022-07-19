7e4fb41532e2d23b848a98410e9af03a957b1a85
Al Clark <alanjclark@users.noreply.github.com>
2022-07-19 18:48:47 +0100

    sdfasd

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,8 +1,13 @@
+const webdriver = require("selenium-webdriver");
+const firefox = require("selenium-webdriver/firefox");
+
 const {Builder, By, Key, until} = require('selenium-webdriver');
 var assert = require('assert');
 
+
 (async function buttonClickTest() {
     let driver = await new Builder().forBrowser('firefox').build();
+    
     try {
         await driver.get('http://localhost:8001');
         await driver.findElement(By.id('button')).click();
